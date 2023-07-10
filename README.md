de9f64cb8e53a01c3af4cfc3f120a47f329d1f95
egusahiroaki <egusa.hiroaki@gmail.com>
2023-07-10 18:06:18 +0900

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+https://thedevelopercafe.com/articles/mocking-interfaces-in-go-with-gomock-670b1a640b00
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+// 👇 an interface acting as API Client
+type ApiClient interface {
+	GetData() string
+}
+
+// 👇 a function using the ApiClient interface
+func Process(client ApiClient) int {
+	data := client.GetData()
+	return len(data)
+}
+
+func main() {
+
+}
